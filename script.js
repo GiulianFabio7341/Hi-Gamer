b1e83715f4a7c990ea34aec6b852cb4c83addb5b
giulian123 <giutrum@gmail.com>
2023-08-21 19:57:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function recarregarpag(){
+    window.location.reload();
+}
+function marioplay(){
+    let a=document.getElementById('mario');
+    a.play();
+    
+    
+}
+function mariopause(){
+    let a=document.getElementById('mario');
+    a.pause();
+}
